862a9e1f82656e286c32fee5d28721cba14a1dee
Alexandra Cojocaru <alexandraflavia00@gmal.com>
2023-01-29 17:49:47 +0200

    edit admin

--- src/main/java/com/example/fooddelivery/controller/UserController.java
+++ src/main/java/com/example/fooddelivery/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.fooddelivery.controller;
 
 
+import com.example.fooddelivery.model.dto.user.AdminUserDto;
 import com.example.fooddelivery.model.dto.user.ClientUserDto;
+import com.example.fooddelivery.service.BaseUserService;
 import com.example.fooddelivery.service.ClientUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +17,11 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
     private final ClientUserService clientUserService;
+    private final BaseUserService baseUserService;
 
-    public UserController(ClientUserService clientUserService) {
+    public UserController(ClientUserService clientUserService, BaseUserService baseUserService) {
         this.clientUserService = clientUserService;
+        this.baseUserService = baseUserService;
     }
 
     @GetMapping("/get-all-clients")
@@ -28,6 +32,15 @@ public class UserController {
     @PutMapping("/update-client/{id}")
     public ResponseEntity<ClientUserDto> editClient(@PathVariable("id") Long id, @RequestBody ClientUserDto dto) {
         ClientUserDto result = clientUserService.editClient(id, dto);
+        if(result == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PutMapping("/update-admin/{id}")
+    public ResponseEntity<AdminUserDto> editAdmin(@PathVariable("id") Long id, @RequestBody AdminUserDto dto) {
+        AdminUserDto result = baseUserService.updateAdmin(id, dto);
         if(result == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
